c3feaa589c2d3cd4a78199a66df89b1c1d9b45dd
nathanachinger <nathan.achinger@ufl.edu>
2023-10-25 20:20:03 -0400

    First commit

---
+++ version_control.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    while True:
+        print("Menu\n-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+        user_input = input("Please enter an option: ")
+        if user_input == '1':
+            password_to_encode = input("Please enter your password to encode: ")
+
